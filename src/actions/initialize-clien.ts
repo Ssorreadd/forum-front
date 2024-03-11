33cd43bdd051c1216eb57a01f69e59cff4e9a337
artcnkk <artcnkkdd@gmail.com>
2024-03-11 03:07:33 +0300

    fix

--- src/actions/initialize-clien.ts
+++ src/actions/initialize-clien.ts
@@ -2,12 +2,10 @@ import {loadCategoryData} from "../storages/category-storage.ts";
 import {loadThisUserData} from "../storages/users-storages.ts";
 
 const initializeClientData = async () => {
-    if (localStorage.getItem('authToken') !== null) {
-        await Promise.all([
-            loadCategoryData(),
-            loadThisUserData(),
-        ])
-    }
+    await Promise.all([
+        loadCategoryData(),
+        loadThisUserData(),
+    ])
 }
 
 export default initializeClientData;
